7bbc9469912a7c06e0b2e872d6ceecf40be1edc5
Enrico Glerean <enrico.glerean@aalto.fi>
2024-05-06 12:07:03 +0300

    making sure edit on github works

--- content/conf.py
+++ content/conf.py
@@ -21,9 +21,9 @@ project = "Container on HPC with Apptainer"
 copyright = "2024-, The contributors"
 author = "The contributors"
 github_user = "coderefinery"
-github_repo_name = ""  # auto-detected from dirname if blank
+github_repo_name = "ttt4hpc_containers"  # auto-detected from dirname if blank
 github_version = "main"
-conf_py_path = "/content/"  # with leading and trailing slash
+conf_py_path = "/ttt4hpc_containers/content/"  # with leading and trailing slash
 
 # -- General configuration ---------------------------------------------------
 
